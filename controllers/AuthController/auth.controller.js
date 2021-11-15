f59c88f672010d63d923f8c769956fae4a1fef99
zeprincelini <princelini4@gmail.com>
2021-11-15 18:33:22 +0100

    auth-login controller added

--- controllers/AuthController/auth.controller.js
+++ controllers/AuthController/auth.controller.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../../models/user");
 
-const RegisterController = async (req, res) => {
+const register = async (req, res) => {
   const { username, email, password, profilePicture, coverPhoto } = req.body;
   try {
     const salt = await bcrypt.genSalt(10);
@@ -33,4 +33,36 @@ const RegisterController = async (req, res) => {
   }
 };
 
-module.exports = { RegisterController };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        res.status("400").json({
+          error: "email/password incorrect",
+        });
+      }
+      const token = jwt.sign(
+        { id: user._id, email: user.email, isAdmin: user.isAdmin },
+        process.env.SECRET
+      );
+      res.status("200").json({
+        status: "success",
+        message: "Login successful",
+        data: {
+          username: user.username,
+          isAdmin: user.isAdmin,
+          accessToken: token,
+        },
+      });
+    } else {
+      res.status("404").json({ error: "user does not exist" });
+    }
+  } catch (err) {
+    res.status("500").json(err);
+  }
+};
+
+module.exports = { register, login };
