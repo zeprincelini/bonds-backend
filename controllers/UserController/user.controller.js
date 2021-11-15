28d2b5c6bd6e062dd1d754b88134079eedefe92f
zeprincelini <princelini4@gmail.com>
2021-11-15 19:16:54 +0100

    unfollow account controller added

--- controllers/UserController/user.controller.js
+++ controllers/UserController/user.controller.js
@@ -86,9 +86,33 @@ const followAccount = async (req, res) => {
   }
 };
 
+const unfollowAccount = async (req, res) => {
+  const id = req.params.id;
+  if (req.body.userId !== id) {
+    try {
+      const user = await User.findById(id);
+      const currentUser = await User.findById(req.body.userId);
+      if (currentUser.following.includes(id)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { following: id } });
+        return res
+          .status("200")
+          .json({ status: "success", message: "user unfollowed successfully" });
+      } else {
+        return res.status("403").json("you don't follow this user");
+      }
+    } catch (err) {
+      return res.status("401").json("user does not exist");
+    }
+  } else {
+    return res.status("403").json({ error: "cannot unfollow yourself" });
+  }
+};
+
 module.exports = {
   updateAccount,
   deleteAccount,
   getAccountById,
   followAccount,
+  unfollowAccount,
 };
--- routes/users.js
+++ routes/users.js
@@ -13,27 +13,6 @@ router.get("/:id", controller.getAccountById);
 
 router.put("/:id/follow", controller.followAccount);
 
-router.put("/:id/unfollow", async (req, res) => {
-  const id = req.params.id;
-  if (req.body.userId !== id) {
-    try {
-      const user = await User.findById(id);
-      const currentUser = await User.findById(req.body.userId);
-      if (currentUser.following.includes(id)) {
-        await user.updateOne({ $pull: { followers: req.body.userId } });
-        await currentUser.updateOne({ $pull: { following: id } });
-        return res
-          .status("200")
-          .json({ status: "success", message: "user unfollowed successfully" });
-      } else {
-        return res.status("403").json("you don't follow this user");
-      }
-    } catch (err) {
-      return res.status("401").json("user does not exist");
-    }
-  } else {
-    return res.status("403").json({ error: "cannot unfollow yourself" });
-  }
-});
+router.put("/:id/unfollow", controller.unfollowAccount);
 
 module.exports = router;
