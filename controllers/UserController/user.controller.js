6433959b7fba9d766207ddc794889387934f50ec
zeprincelini <princelini4@gmail.com>
2021-11-15 19:13:44 +0100

    follow account controller added

--- controllers/UserController/user.controller.js
+++ controllers/UserController/user.controller.js
@@ -63,4 +63,32 @@ const getAccountById = async (req, res) => {
   }
 };
 
-module.exports = { updateAccount, deleteAccount, getAccountById };
+const followAccount = async (req, res) => {
+  const id = req.params.id;
+  if (req.body.userId !== id) {
+    try {
+      const user = await User.findById(id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!currentUser.following.includes(id)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: id } });
+        return res
+          .status("200")
+          .json({ status: "success", message: "user followed successfully" });
+      } else {
+        return res.status("403").json("you already follow this user");
+      }
+    } catch (err) {
+      return res.status("401").json("user does not exist");
+    }
+  } else {
+    return res.status("403").json({ error: "cannot follow yourself" });
+  }
+};
+
+module.exports = {
+  updateAccount,
+  deleteAccount,
+  getAccountById,
+  followAccount,
+};
--- routes/users.js
+++ routes/users.js
@@ -11,28 +11,7 @@ router.delete("/:id", controller.deleteAccount);
 
 router.get("/:id", controller.getAccountById);
 
-router.put("/:id/follow", async (req, res) => {
-  const id = req.params.id;
-  if (req.body.userId !== id) {
-    try {
-      const user = await User.findById(id);
-      const currentUser = await User.findById(req.body.userId);
-      if (!currentUser.following.includes(id)) {
-        await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({ $push: { following: id } });
-        return res
-          .status("200")
-          .json({ status: "success", message: "user followed successfully" });
-      } else {
-        return res.status("403").json("you already follow this user");
-      }
-    } catch (err) {
-      return res.status("401").json("user does not exist");
-    }
-  } else {
-    return res.status("403").json({ error: "cannot follow yourself" });
-  }
-});
+router.put("/:id/follow", controller.followAccount);
 
 router.put("/:id/unfollow", async (req, res) => {
   const id = req.params.id;
