350ed5fe49f42982e9410c1c555c07d77642698d
zeprincelini <princelini4@gmail.com>
2022-03-15 23:52:17 +0100

    .

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const commentRoute = require("./routes/comment");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
